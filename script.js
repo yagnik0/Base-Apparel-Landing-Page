94d52c0b50c70403bd9225f2940ae0efd0c37299
yagnik0 <yashyagnik456@.com>
2023-12-28 18:05:25 +0530

    optimize code

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ const errorIcon = document.querySelector('.error')
 const email = document.querySelector('.email')
 const input = document.querySelector('.input-flex')
 const btn = document.querySelector('.btn')
+const inputID = document.getElementById('inputid')
 
 btn.addEventListener('click', function(){
     if (!email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-        retryMessage.classList.toggle('hidden')
-        errorIcon.classList.toggle('hidden')
+        retryMessage.classList.remove('hidden')
+        errorIcon.classList.remove('hidden')
+        inputID.style.border = "2px solid hsl(0, 93%, 68%)"
+    } else {
+        retryMessage.classList.add('hidden')
+        errorIcon.classList.add('hidden')
     }
 })
 
